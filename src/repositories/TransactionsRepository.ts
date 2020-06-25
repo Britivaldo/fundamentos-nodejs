10df3c3418b3fde57a90a0ebd984256372553544
Britivaldo Neto <britoleopoldo21@gmail.com>
2020-06-25 20:57:32 -0300

    Create/List/Valid

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,68 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulador: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulador.income += transaction.value;
+            break;
+          case 'outcome':
+            // eslint-disable-next-line no-param-reassign
+            accumulador.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return accumulador;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+    /*
+    const income = this.transactions
+      .map(transction => {
+        if (transction.type === 'income') {
+          return transction.value;
+        }
+        return 0;
+      })
+      .reduce((sum, value) => sum + value);
+
+    const outcome = this.transactions
+      .map(transction => {
+        if (transction.type === 'outcome') {
+          return transction.value;
+        }
+        return 0;
+      })
+      .reduce((sum, value) => sum + value);
+
+    const total = income - outcome;
+      */
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const { income, outcome } = this.getBalance();
+
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
